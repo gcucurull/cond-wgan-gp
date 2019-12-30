94b9b5819f2c565e729dbde7028b086392ce081f
Guillem Cucurull <gcucurull@gmail.com>
2019-12-30 18:58:04 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cond-wgan-gp
-Pytorch implementation of a Conditional WGAN with Gradient Penalty
+# Pytorch Conditional WGAN with Gradient Penalty
+Pytorch implementation of a Conditional [WGAN](https://arxiv.org/abs/1701.07875) with [Gradient Penalty (GP)](https://arxiv.org/abs/1704.00028).
+
+This implementation is based on the Conditional GAN and WGAN-GP implementations from this [amazing repository](https://github.com/eriklindernoren/PyTorch-GAN) with many different GAN implementations.
+
+# Usage
+Just run
+
+```
+python main.py
+```
+
+It will create an `images` directory and save generated images every few iterations.
---
+++ models.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, opt):
+        super(Generator, self).__init__()
+        self.opt = opt
+
+        self.label_emb = nn.Embedding(opt.n_classes, opt.n_classes)
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim + opt.n_classes, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(opt.img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z, labels):
+        # Concatenate label embedding and image to produce input
+        gen_input = torch.cat((self.label_emb(labels), z), -1)
+        img = self.model(gen_input)
+        img = img.view(img.shape[0], * self.opt.img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, opt):
+        super(Discriminator, self).__init__()
+
+        self.label_embedding = nn.Embedding(opt.n_classes, opt.n_classes)
+
+        # Original model
+        # self.model = nn.Sequential(
+        #     nn.Linear(int(np.prod(img_shape)), 512),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(512, 256),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(256, 1),
+        # )
+
+        # Copied from cgan.py
+        self.model = nn.Sequential(
+            nn.Linear(opt.n_classes + int(np.prod(opt.img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1),
+        )
+
+    def forward(self, img, labels):
+        # Concatenate label embedding and image to produce input
+        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
+        validity = self.model(d_in)
+        return validity
